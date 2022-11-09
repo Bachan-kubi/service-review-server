7884ab6479edd15825ff03811b0a9f3ddc629e77
Bachan-kubi <felixghagra@gmail.com>
2022-11-09 10:14:28 +0600

    get and post method added

--- index.js
+++ index.js
@@ -63,8 +63,88 @@ async function run(){
             // console.log(user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '10h'});
             res.send({token});
-
         })
+        // to get 3 data in in browser 
+        app.get('/services', async (req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        // to get all data in browser 
+        app.get('/allservices', async (req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        // to get specific data/product or activities in browser
+        app.get('/services/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        // get orders in ui
+        app.get('/orders', verifyJWT, async (req, res)=>{
+            // jwt token 
+            const decoded= req.decoded;
+            console.log('inside orders api', decoded);
+            if(decoded.email !==req.query.email){
+                return res.status(403).send({message: 'Unauthorized Access!'})
+            }
+
+            // console.log(req.headers.authorization)
+
+            // console.log(req.query.email); query by email/can be found by id
+            let query= {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+        // order api to create a order sheet in collections
+        app.post('/orders', async (req, res)=>{
+            const order= req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result);
+        });
+        // updata orders
+        app.patch('/orders/:id', async (req, res)=>{
+            const id= req.params.id;
+            const status = req.body.status
+            const query = {_id: ObjectId(id)};
+            const updateDocs ={
+                $set: {
+                    status: status
+                }
+            }
+            const resutl = await ordersCollection.updateOne(query, updateDocs);
+            res.send(resutl)
+        })
+        // delete operations 
+        // app.delete('/orders/:id', async (req, res)=>{
+        //     const id = req.params.id;
+        //     console.log(id)
+        //     const query = {_id: ObjectId(id)}
+        //     const result = await ordersCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+    }
+         // 3.4
+    finally{
+
+    }
+}
+// 3.2
+run().catch(error=>console.error(error));
+
 
 
 
